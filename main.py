a5c1b864eda9451e19039b3532fdf9e6d9a6c8cf
AmbF <90185021+AmbroiseFrey@users.noreply.github.com>
2021-11-20 18:42:02 +0000

    Embellissement du code, travail sur pygame

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ import pygame
 # Couleurs de base
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (144,238,144)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
 #Setup de la fenetre pygame
 pygame.init()
 screen = pygame.display.set_mode([700, 500])
+pygame.display.set_caption("Projet")
+RUN = True
 
+#Login
 user = input('User: ')
 password = input('Password: ')
 logged_in = False
+
+
+##----------------------------------------------------------------------##
+##Calculs et fonctionnement de notre ordinateur (si on garde cette idée)##
+##----------------------------------------------------------------------##
 
 
 class OperatingSystem:
@@ -27,21 +37,32 @@ class OperatingSystem:
     else:
       print('Wrong !')
       return
-  
-  def mails():
-    return
 
-while True:
-  screen.fill(BLACK)
+
+
+##------------------------------##
+##---Boucle Principale du Jeu---##
+##------------------------------##
+
+while RUN:
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      RUN= False
+  
+  screen.fill(YELLOW) #Bug ?
+
+
   if not (logged_in):
     os = OperatingSystem(user, password)
     logged_in = True
+  
   action = input('Command:')
   if action == '-randompage':
     load_page(random_page())
+    action = ''
   if action == '-search':
     page = input('Search:')
     load_page(page)
-    break
-
+    action = ''
 pygame.quit()
