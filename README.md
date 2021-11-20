d10d42c4bf6817d05ad64dd73aa47cd82926d7b6
AmbF <90185021+AmbroiseFrey@users.noreply.github.com>
2021-11-20 16:01:19 +0000

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project-NSI
 Via https://replit.com/@AmbF/Project
+</br>
+Pour pouvoir travailler sur le Repl.it:
+</br>
+* https://replit.com/join/nowchtkgzn-ambf
--- main.py
+++ main.py
@@ -5,19 +5,16 @@ user = input('User: ')
 password = input('Password: ')
 logged_in = False
 
-s=web_search
+s = web_search
 
 while True:
-  if not(logged_in):
-    os = OperatingSystem(user, password)
-    logged_in = True
+  if not (logged_in):
+      os = OperatingSystem(user, password)
+      logged_in = True
   action = input('Command:')
   if action == '-randompage':
-    s.load_page(s.random_page())
+      s.load_page(s.random_page())
   if action == '-search':
-    page = input('Search:')
-    s.load_page(page)
-    break
-  
-  
-  
+      page = input('Search:')
+      s.load_page(page)
+      break
