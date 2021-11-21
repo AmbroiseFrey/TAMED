07ca020516aaf8ba98ec1d0b022427f2b90384d3
AmbF <90185021+AmbroiseFrey@users.noreply.github.com>
2021-11-21 08:43:18 +0000

    Travail sur class OperatingSystem

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from web_search import *
+import web_search
 import pygame
 
+s = web_search
 
-# Couleurs de base
+# Couleurs de base - un tuple (R,V,B)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
@@ -13,10 +14,10 @@ YELLOW = (255, 255, 0)
 #Setup de la fenetre pygame
 pygame.init()
 screen = pygame.display.set_mode([700, 500])
-screen.fill(BLACK)
 pygame.display.set_caption("Projet")
+screen.fill(RED)
 RUN = True
-logged_in = False
+os = False
 
 
 
@@ -25,17 +26,18 @@ logged_in = False
 ##Calculs et fonctionnement de notre ordinateur (si on garde cette idée)##
 ##----------------------------------------------------------------------##
 
-
 class OperatingSystem:
-  def __init__(self):
+
+  def log_in():
+    '''Fonction qui demande un username et un passcode. Les seuls valides pour l'instant son User: User1 et Password: 0000 '''
     user = input('User: ')
     password = input('Password: ')
     if password == '0000' and user == 'User1':
       print('Welcome back ' + user + '!')
-      return
+      return True
     else:
       print('Wrong !')
-      return
+      return False
 
 
 
@@ -47,19 +49,21 @@ while RUN:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       RUN= False
+    
+    #Si clavier
     if event.type == pygame.KEYDOWN:
-      if not(logged_in):
-        os = OperatingSystem()
-        logged_in = True
-  
-      action = input('Command:')
-      if action == '-randompage':
-        load_page(random_page())
-        action = ''
-      if action == '-search':
-        page = input('Search:')
-        load_page(page)
-        action = ''
+      if not(os):
+        os = OperatingSystem.log_in()
+      elif os:
+        screen.fill(RED)
+        action = input('Command:')
+        if action == '-randompage':
+          s.load_page(s.random_page())
+          action = ''
+        if action == '-search':
+          page = input('Search:')
+          s.load_page(page)
+          action = ''
 
 
 pygame.quit()
