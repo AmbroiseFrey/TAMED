eb5ca61f791d6d6b41f2748ec62418de710d61e8
AmbF <90185021+AmbroiseFrey@users.noreply.github.com>
2021-11-21 14:53:04 +0000

    ⭕Ajout de la fonction render_circle()

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ user_logged = False
 
 
 
-##----------------------------------------------------------------------##
-##Calculs et fonctionnement de notre ordinateur (si on garde cette idée)##
-##----------------------------------------------------------------------##
+##--------------------------------------------------------------------------##
+##--Calculs et fonctionnement de notre ordinateur (si on garde cette idée)--##
+##----------------Sinon, toujours utile d'avoir une classe------------------##
+##Qui comprend toutes les fonctions qui facilitent l'interaction avec pygame##
+##--------------------------------------------------------------------------##
 
 class OperatingSystem:
 
@@ -44,8 +46,8 @@ class OperatingSystem:
 
 
   def render_text(text,pos):
-    assert type(text) == str, 'Le text doit etre un string !'
-    assert type(pos) == tuple, 'La pos doit etre un tuple !'
+    assert type(text) == str, 'Le texte doit etre un string !'
+    assert type(pos) == tuple, 'La position doit etre un tuple !'
     pygame.font.init()
     font = pygame.font.SysFont('Comic Sans MS', 30)
     text = font.render(text, False, (255, 255, 255))
@@ -56,7 +58,7 @@ class OperatingSystem:
     '''Fonction qui permet d\'afficher une image. Prend en argument le nom de l'image (str), sa position (tuple), et sa taille (tuple)'''
     assert type(image_name) == str, 'Le nom de l\'image doit etre un string !'
     assert type(pos) == tuple, 'La position doit etre un tuple !'
-    assert type(pos) == tuple, 'La taille doit etre un tuple !'
+    assert type(size) == tuple, 'La taille doit etre un tuple !'
     loaded_img= pygame.image.load(image_name)
     loaded_img = pygame.transform.scale(loaded_img, size)
     screen.blit(loaded_img, pos + size)
@@ -64,7 +66,17 @@ class OperatingSystem:
 
   def render_rectangle(color,size,pos):
     '''Fonction qui permet d\'afficher un rectangle. Prend en argument la couleur (un tuple), sa taille (tuple), et sa position (tuple)'''
+    assert type(pos) == tuple, 'La position doit etre un tuple !'
+    assert type(size) == tuple, 'La taille doit etre un tuple !'
     pygame.draw.rect(screen, color, pygame.Rect(pos + size))
+
+
+  def render_circle(color,radius,pos):
+    assert type(pos) == tuple, 'La position doit etre un tuple !'
+    assert type(radius) == int, 'Le rayon doit etre un chiffre !'
+    '''Fonction qui permet d\'afficher un cercle. Prend en argument la couleur (un tuple), son rayon (int), et sa position (tuple)'''
+    pygame.draw.circle(screen, color, pos, radius)
+
 
   def check_interaction(clickpos, wantedarea):
     '''Fonction qui prend en parametre la position de la souris au moment du click que l'on check et qui la compare avec la zone que l'on veut sous forme de tuple - (x1, x2,y1,y2). La fonction renvoi True ou False selon si la souris est bien a l'endrtoi voulu'''
@@ -94,6 +106,7 @@ while RUN:
     screen.fill(RED)
     Os.render_rectangle(WHITE, (20,30), (0,300))
     Os.render_text('Loading...',(0,0))
+    Os.render_circle(BLACK,20,(100,100))
   
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
